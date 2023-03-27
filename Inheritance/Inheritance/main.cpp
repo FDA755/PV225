2b68991fbe93ecae37db444305f9cb2e80e61f62
svaadmin <svaadmin@DESKTOP-0PCLR79>
2023-03-27 15:56:54 +0300

    inher

--- Inheritance/Inheritance/main.cpp
+++ Inheritance/Inheritance/main.cpp
@@ -1,9 +1,33 @@
-//Inheritance
 #include<iostream>
 using namespace std;
+
+class Animal
+{
+public:
+	virtual void sound() = 0;
+};
+
+class Cat :public Animal
+{
+public:
+	void sound()
+	{
+		cout << "ּÿף" << endl;
+	}
+};
+class Dog :public Animal
+{
+public:
+	void sound()
+	{
+		cout << "ֳאג" << endl;
+	}
+};
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Inheritance!\n";
+	Cat tom; tom.sound();
+
+	Dog hatico; hatico.sound();
 }
